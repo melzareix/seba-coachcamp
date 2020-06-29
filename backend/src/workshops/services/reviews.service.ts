351ea1b6da639fb8f025f3d9790e8915b8d39150
Youssef <joex3999@gmail.com>
2020-06-29 11:58:24 +0200

    feat: add reviews dependancies and seed file

---
+++ backend/src/workshops/services/reviews.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Review } from '../models/review.model';
+import { InjectModel } from 'nestjs-typegoose';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { ReviewCreateDto } from '../reviews.types';
+
+@Injectable()
+export class ReviewsService {
+  constructor(
+    @InjectModel(Review)
+    private readonly reviewModel: ReturnModelType<typeof Review>,
+  ) {}
+
+  async create(review: ReviewCreateDto): Promise<Review> {
+    return await this.reviewModel.create(review);
+  }
+
+  async update(id: string, review: ReviewCreateDto): Promise<Review> {
+    return await this.reviewModel.findByIdAndUpdate(id, review);
+  }
+
+  async findAll(): Promise<Review[]> {
+    return await this.reviewModel.find().exec();
+  }
+
+  async findReviewsForInstructor(instructorId: string): Promise<Review[]> {
+    return await this.reviewModel.find({"instructor":instructorId});
+  }
+
+
+}
